5a22c47a831c6e5f9af381e4915dcc249caec402
keviny1981 <pluxy@naver.com>
2024-05-13 14:20:35 +0900

    Update TC
    - to validate network response upon successful account creation
    - Added some comments to outline its code's purpose

--- 3. TC_0001_sign_up_with_valid_account.spec.ts
+++ 3. TC_0001_sign_up_with_valid_account.spec.ts
@@ -1,9 +1,4 @@
-import { test, expect, chromium, BrowserContext } from '@playwright/test';
-import Env from '../../lib/helpers/env';
-import { config } from '../../lib/helpers/config';
-
-// Import commonly used functions
-import * as common from './common';
+import { test, expect, chromium } from '@playwright/test';
 import fs from 'fs';
 
 // Adjust test timeout
@@ -18,17 +13,16 @@ async function takeScreenshot(page: any, fileName: string) {
 
   // Create root directory if it doesn't exist
   const rootDirectoryPath: string = 'screenshots';
-  if (!fs.existsSync(rootDirectoryPath))
-    fs.mkdirSync(rootDirectoryPath);
+  if (!fs.existsSync(rootDirectoryPath)) fs.mkdirSync(rootDirectoryPath);
 
   const subDirectoryPath: string = rootDirectoryPath + '/' + uniqueTimeStamp;
 
   // Create sub-directory if it doesn't exist
-  if (!fs.existsSync(subDirectoryPath))
-    fs.mkdirSync(subDirectoryPath);
+  if (!fs.existsSync(subDirectoryPath)) fs.mkdirSync(subDirectoryPath);
 
   // Create unique screenshot filename
-  const filePath: string = subDirectoryPath + '/' + uniqueTimeStamp + '-00_' + fileName + '_' + '.png';
+  const filePath: string =
+    subDirectoryPath + '/' + uniqueTimeStamp + '-00_' + fileName + '_' + '.png';
 
   // Save screenshot
   await page.screenshot({ path: filePath });
@@ -37,7 +31,7 @@ async function takeScreenshot(page: any, fileName: string) {
 test('Signn up with valid account', async () => {
   // Open a new browser
   const browser = await chromium.launch();
-  const context = await browser.newContext({ acceptDownloads: true, });
+  const context = await browser.newContext({ acceptDownloads: true });
   const page = await context.newPage();
 
   // Adjust page size
@@ -46,6 +40,10 @@ test('Signn up with valid account', async () => {
   // Move to target url
   await page.goto('http://13.209.85.69/');
 
+  // Wait until page gets loaded
+  await page.waitForLoadState();
+
+  // Validate availability of each fields
   const userName = page.getByLabel('Username:');
   await expect(userName).toBeVisible();
 
@@ -64,19 +62,38 @@ test('Signn up with valid account', async () => {
   // Fill out user name
   await userName.click();
   await userName.fill('qa_test_001');
+  await page.waitForFunction(() => new Promise((resolve) => setTimeout(resolve, 1000)));
 
   // Fill out password
   await password.click();
   await password.fill('Aaa12345!');
+  await page.waitForFunction(() => new Promise((resolve) => setTimeout(resolve, 1000)));
 
   await email.click();
   await email.fill('qa_test_001@gmail.com');
-  
+  await page.waitForFunction(() => new Promise((resolve) => setTimeout(resolve, 1000)));
+
   // Opt-in subscribe
   await page.getByLabel('Subscribe to newsletter').check();
-  
+  await page.waitForFunction(() => new Promise((resolve) => setTimeout(resolve, 1000)));
+
   // Submit sign up form
   await registerButton.click();
+  await page.waitForFunction(() => new Promise((resolve) => setTimeout(resolve, 1000)));
+
+  // Make sure that we have response 200
+  let isSuccessResponseObserved = false;
+  page.on('response', async (response) => {
+    // Specify the URL or part of it you're interested in
+    if (response.url().includes('13.209.85.69')) {
+      // Check if the status code is 200
+      if (response.status() === 200) {
+        isSuccessResponseObserved = true;
+      }
+    }
+  });
+
+  expect(isSuccessResponseObserved).toBe(true);
 
   // Take screenshot
   await takeScreenshot(page, 'sign_up_with_valid_account');
